2ca3f7d264139c3427d533a50cc85180b9ad2776
DavidDelg <137916665+DavidDelg@users.noreply.github.com>
2026-04-07 13:29:55 -0600

    scrip de nm

--- docJs/script.js
+++ docJs/script.js
@@ -30,22 +30,29 @@ function agregarNumero(){
   let num = parseFloat(document.getElementById("numero").value);
   let lista = document.getElementById("lista");
   let promedios = document.getElementById("promedios");
-  return;
-}
-
-numeros.push(num);
-
-//clasificar numero
-if(Number.isInteger(num)){
-  if(num % 2 === 0){
-    pares.push(num);
-  }else{
-    impares.push(num);
+  
+  if (isNaN(num)) {
+    lista.textContent = "Por favor ingrese un numero válido";
+    lista.style.color = red;
+    return;
   }
-}
+  numeros.push(num);
+//clasificar numero
+  if(Number.isInteger(num)){
+    if(num % 2 === 0){
+      pares.push(num);
+    }else{
+      impares.push(num);
+    }
+  }
 //Mostrar lista acumulada
 lista.textContent = "Numeros ingresados: ${numeros.join(", ")}";
 
+//Calcular promedios
+let promedioPares = pares.length > 0 ? (pares.reduce((a, b) => a+b, 0) / pares.length).toFixed(2) : 0;
+let promedioImpares = impares.length > 0 ? (impares.reduce((a,b) => a+b, 0) / impares.length).toFixed(2) : 0;
+
+promedios.textContent = 'Promedios pares: ${promediosPares} | Promedios impares: ${promedioImpares}';
 
 
-
+}
