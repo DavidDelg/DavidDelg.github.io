c0112e15129609843c058823547e6bd862554b68
DavidDelg <137916665+DavidDelg@users.noreply.github.com>
2026-04-07 12:52:24 -0600

    scrip aut

--- docJs/script.js
+++ docJs/script.js
@@ -1,3 +1,5 @@
+
+//Codigo de menu lateral cuando la pantalla es para movil
 const btnMenu = document.getElementById('btn-menu');
 const sidebar = document.querySelector('.sidebar');
 
@@ -19,3 +21,31 @@ links.forEach(link => {
   });
 
 });
+//Codigo del contador y promedio
+let numeros = [];
+let pares = [];
+let impares = [];
+
+function agregarNumero(){
+  let num = parseFloat(document.getElementById("numero").value);
+  let lista = document.getElementById("lista");
+  let promedios = document.getElementById("promedios");
+  return;
+}
+
+numeros.push(num);
+
+//clasificar numero
+if(Number.isInteger(num)){
+  if(num % 2 === 0){
+    pares.push(num);
+  }else{
+    impares.push(num);
+  }
+}
+//Mostrar lista acumulada
+lista.textContent = "Numeros ingresados: ${numeros.join(", ")}";
+
+
+
+
